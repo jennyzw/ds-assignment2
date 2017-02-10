1fb30a7d4482cb4cedc8dc0d6b508270e9ee1f75
Duncan Hall <duncan.hall@students.olin.edu>
2017-02-10 13:01:10 -0500

    interfaces 1.0

--- src/client_int.java
+++ src/client_int.java
@@ -2,4 +2,5 @@
  * Created by duncan on 2/10/17.
  */
 public interface client_int {
+    void pushMessage(String msg);
 }
--- src/server_int.java
+++ src/server_int.java
@@ -1,5 +1,11 @@
+import java.rmi.Remote;
+
 /**
  * Created by duncan on 2/10/17.
  */
-public interface server_int {
+public interface server_int extends Remote {
+    boolean registerName(String name);
+    boolean sendMessage(String user, String msg);
+    String requestDirectory();
+    void quit();
 }
