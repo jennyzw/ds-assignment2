028e49c9f24952bff6990aa4dae12a36f631ac11
Duncan Hall <duncan.hall@students.olin.edu>
2017-02-13 17:29:00 -0500

    server updated

--- src/test/Server.java
+++ src/test/Server.java
@@ -4,6 +4,8 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Created by duncan on 2/10/17.
@@ -11,28 +13,55 @@ import java.rmi.server.UnicastRemoteObject;
  */
 public class Server implements Server_int {
 
+    private HashMap<String, Client_int> users;
 
-
-    public Server() {}
-
-    public boolean registerName(String name) throws RemoteException {
-        return false;
+    public Server() {
+        users = new HashMap<String, Client_int>();
     }
 
+    @Override
+    public boolean registerName(String name, Client_int stub) throws RemoteException {
+        if (users.containsKey(name)) {
+            return false;
+        } else {
+            users.put(name, stub);
+            return true;
+        }
+    }
+
+    @Override
     public boolean sendMessage(String user, String msg) throws RemoteException {
-        return false;
+        if (users.containsKey(user)) {
+            users.get(user).pushMessage(msg);
+            return true;
+        } else {
+            // user doesn't exist, let client know
+            return false;
+        }
     }
 
+    @Override
     public String requestDirectory() throws RemoteException {
-        return null;
+        return users.keySet().toString();
     }
 
-    public void quit() throws RemoteException {
-
-    }
-
+    @Override
     public String sayHello() {
-        return "hello";
+        return "welcome to our basic IM thing!";
+    }
+
+    private static boolean executeCommand(String input) {
+        String arr[] = input.split(" ", 2);
+        String command = arr[0];
+        String rest = arr[1];
+
+        switch (command) {
+            case "quit":
+                return false;
+            default:
+                System.out.println("unknown command");
+        }
+        return true;
     }
 
     public static void main(String args[]) {
@@ -46,11 +75,22 @@ public class Server implements Server_int {
             registry.bind("im_server", stub);
 
             System.err.println("Server ready");
+
+            Scanner sc = new Scanner(System.in);
+            String input;
+            while (true) {
+                input = sc.next();
+                if (!executeCommand(input)) break;
+            }
+
+            System.err.println("Server shutting down...");
+
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
         }
     }
 
-
 }
+
+
